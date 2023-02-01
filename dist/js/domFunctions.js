26deeccd816b2e2514fd74f2e599ba86f2808f7d
Dev-EndLess <endlesspain1988@gmail.com>
2023-02-01 12:57:46 +0100

    create forecastIcon and logic, update img/gif dir

--- dist/js/domFunctions.js
+++ dist/js/domFunctions.js
@@ -48,7 +48,7 @@ const updateWeatherLocationHeader = (message) => {
     const lon = mapArray[1].indexOf("-") === -1
       ? mapArray[1].slice(0, 10)
       : mapArray[1].slice(0, 11)
-    h1.textContent = `♦ ${lat} ♦ ${lon}`
+    h1.textContent = `• ${lat} ♦ ${lon} •`
   } else {
     h1.textContent = message
   }
@@ -69,6 +69,7 @@ export const updateDisplay = (weatherJson, locationObj) => {
   // current conditions
   const currentConditionArray = createCurrentConditionsDivs(weatherJson, locationObj.getUnit())
   displayCurrentConditions(currentConditionArray)
+  displaySixDayForecast(weatherJson)
   // six day forecast
   setFocusOnSearch()
   fadeDisplay()
@@ -99,22 +100,26 @@ const deleteContents = (parentElement) => {
 }
 
 const getWeatherClass = (icon) => {
+  const firstChar = icon.slice(0, 1)
   const firstTwoChars = icon.slice(0, 2)
   const lastChar = icon.slice(2)
   const weatherLookup = {
-    "09": "soft rain",
-    "10": "heavy rain",
-    "11": "thunder storm",
+    "02": "few-clouds",
+    "03": "few-clouds",
+    "04": "broken-clouds",
+    "09": "heavy-rain",
+    "10": "light-rain",
+    "11": "thunder-storm",
     "13": "snow",
-    "50": "fog"
+    "50": "fog",
   }
   let weatherClass
   if (weatherLookup[firstTwoChars]) {
     weatherClass = weatherLookup[firstTwoChars]
-  } else if (lastChar === "d") {
-    weatherClass = "clouds"
-  } else {
+  } else if (firstChar === "0" && lastChar === "n") {
     weatherClass = "night"
+  } else {
+    weatherClass = "clear-sky"
   }
   return weatherClass
 }
@@ -147,21 +152,21 @@ const createCurrentConditionsDivs = (weatherObj, unit) => {
   const temp = createElement("div", "temp", `${Math.round(Number(weatherObj.current.temp))}°`, tempUnit)
   const properDescription = toProperCase(weatherObj.current.weather[0].description)
   const description = createElement("div", "desc", properDescription)
-  const feels = createElement("div", "feels", `Feels Like ${Math.round(Number(weatherObj.current.feels_like))}°`)
-  const maxTemp = createElement("div", "maxTemp", `High ${Math.round(Number(weatherObj.daily[0].temp.max))}°`)
-  const minTemp = createElement("div", "minTemp", `Low ${Math.round(Number(weatherObj.daily[0].temp.min))}°`)
-  const humidity = createElement("div", "humidity", `Humidity ${Math.round(Number(weatherObj.current.humidity))}%`)
-  const wind = createElement("div", "wind", `Wind ${Math.round(Number(weatherObj.current.wind_speed))} ${windUnit}`)
+  const feels = createElement("div", "feels", `Temp. Percepita ${Math.round(Number(weatherObj.current.feels_like))}°`)
+  const maxTemp = createElement("div", "maxtemp", `Massime ${Math.round(Number(weatherObj.daily[0].temp.max))}°`)
+  const minTemp = createElement("div", "mintemp", `Minime ${Math.round(Number(weatherObj.daily[0].temp.min))}°`)
+  const humidity = createElement("div", "humidity", `Umidità ${Math.round(Number(weatherObj.current.humidity))}%`)
+  const wind = createElement("div", "wind", `Venti ${Math.round(Number(weatherObj.current.wind_speed))} ${windUnit}`)
   return [icon, temp, description, feels, maxTemp, minTemp, humidity, wind]
 }
 
 const createMainImgDiv = (icon, altText) => {
   const iconDiv = createElement("div", "icon")
   iconDiv.id = "icon"
-  const fontAwesomeIcon = translateIconToFontAwesome(icon)
-  fontAwesomeIcon.ariaHidden = true
-  fontAwesomeIcon.title = altText
-  iconDiv.appendChild(fontAwesomeIcon)
+  const myIcon = translateIconToMyIcon(icon)
+  myIcon.ariaHidden = true
+  myIcon.title = altText
+  iconDiv.appendChild(myIcon)
   return iconDiv
 }
 
@@ -180,54 +185,54 @@ const createElement = (elementType, divClassName, divText, unit) => {
   return div
 }
 
-const translateIconToFontAwesome = (icon) => {
-  const i = document.createElement("i")
+const translateIconToMyIcon = (icon) => {
+  const img = document.createElement("img")
   const firstTwoChars = icon.slice(0, 2)
   const lastChar = icon.slice(2)
   switch (firstTwoChars) {
     case "01":
       if (lastChar === "d") {
-        i.classList.add("far", "fa-sun")
+        img.src = `./animated-icons/clear-day.svg`
       } else {
-        i.classList.add("far", "fa-moon")
+        img.src = `./animated-icons/clear-night.svg`
       }
       break;
     case "02":
       if (lastChar === "d") {
-        i.classList.add("fas", "fa-cloud-sun")
+        img.src = `./animated-icons/partly-cloudy-day.svg`
       } else {
-        i.classList.add("fas", "fa-cloud-moon")
+        img.src = `./animated-icons/partly-cloudy-night.svg`
       }
       break;
     case "03":
-      i.classList.add("fas", "fa-cloud")
+      img.src = `./animated-icons/overcast.svg`
       break;
     case "04":
-      i.classList.add("fas", "fa-cloud-meatball")
-      break;
+      img.src = `./animated-icons/extreme.svg`
+     break;
     case "09":
-      i.classList.add("fas", "fa-cloud-rain")
+      img.src = `./animated-icons/extreme-rain.svg`
       break;
     case "10":
       if (lastChar === "d") {
-        i.classList.add("fas", "fa-cloud-sun-rain")
+        img.src = `./animated-icons/partly-cloudy-day-rain.svg`
       } else {
-        i.classList.add("fas", "fa-cloud-moon-rain")
+        img.src = `./animated-icons/partly-cloudy-night-rain.svg`
       }
       break;
     case "11":
-      i.classList.add("fas", "fa-poo-storm")
+      img.src = `./animated-icons/thunderstorms-extreme.svg`
       break;
     case "13":
-      i.classList.add("far", "fa-snowflake")
+      img.src = `./animated-icons/overcast-snow.svg`
       break;
     case "50":
-      i.classList.add("fas", "fa-smog")
+      img.src = `./animated-icons/overcast-fog.svg`
       break;
     default:
-      i.classList.add("far", "fa-question-circle")
+      img.src = `./animated-icons/clear-night.svg`
   }
-  return i
+  return img
 }
 
 const displayCurrentConditions = (currentConditionArray) => {
@@ -235,4 +240,62 @@ const displayCurrentConditions = (currentConditionArray) => {
   currentConditionArray.forEach((currentCondition) => {
     currentConditionContainer.appendChild(currentCondition)
   })
+}
+
+const displaySixDayForecast = (weatherJson) => {
+  for (let i = 1; i <= 6; i++) {
+    const displayForecastArray = createDailyForecastDivs(weatherJson.daily[i])
+    displayDailyForecast(displayForecastArray)
+  }
+}
+
+const createDailyForecastDivs = (dayWeather) => {
+  const dayAbbreviationText = getDayAbbreviation(dayWeather.dt)
+  const dayAbbreviation = createElement(
+    "p",
+    "dayAbbreviation",
+    dayAbbreviationText
+  )
+  const dayIcon = createDailyForecastIcon(
+    dayWeather.weather[0].icon,
+    dayWeather.weather[0].description
+  )
+  const dayHigh = createElement(
+    "p",
+    "dayHigh",
+    `${Math.round(Number(dayWeather.temp.max))}°`
+  )
+  const dayLow = createElement(
+    "p",
+    "dayLow",
+    `${Math.round(Number(dayWeather.temp.min))}°`
+  )
+  return [dayAbbreviation, dayIcon, dayHigh, dayLow]
+}
+
+const getDayAbbreviation = (data) => {
+  const dateObj = new Date(data * 1000)
+  const utcString = dateObj.toUTCString()
+  return utcString.slice(0, 3).toUpperCase()
+}
+
+const createDailyForecastIcon = (icon, altText) => {
+  const iconDiv = createElement("div", "icon")
+  iconDiv.id = "icon"
+  const myIcon = translateIconToMyIcon(icon)
+  myIcon.ariaHidden = true
+  myIcon.title = altText
+  iconDiv.appendChild(myIcon)
+  return iconDiv
+}
+
+const displayDailyForecast = (displayForecastArray) => {
+  const dayDiv = createElement("div", "forecastDay")
+  displayForecastArray.forEach((element) => {
+    dayDiv.appendChild(element)
+  })
+  const dailyForecastContainer = document.getElementById(
+    "dailyForecast--contents"
+  )
+  dailyForecastContainer.appendChild(dayDiv)
 }
